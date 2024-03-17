9c158c3eccb2d7100de955beb225790c707ea279
sonia-o <sonicu.olar@gmail.com>
2024-03-17 10:54:02 -0400

    Implemented bubble sort

--- src/com/dm/sort/BubbleSortExample.java
+++ src/com/dm/sort/BubbleSortExample.java
@@ -6,25 +6,43 @@ public class BubbleSortExample {
 
 	public static void main(String[] args) {
 		int[] numbersArray = new int[30];
-		
+
 		for (int i = 0; i < 30; i++) {
 			numbersArray[i] = (int) (99 * Math.random());
 		}
-		
-		printArrayOfNumbers(numbersArray);
 
+		printArrayOfNumbers(numbersArray);
+		for (int p = 0; p < numbersArray.length; p++) {
+			for (int i = 0; i < (numbersArray.length - 1); i++) {
+				int value1 = numbersArray[i];
+				int value2 = numbersArray[i + 1];
+				if (value1 > value2) {
+					numbersArray[i] = value2;
+					numbersArray[i + 1] = value1;
+				}
+			}
+		}
 		// sort numbers ascending
 		// ... include code here
 		printArrayOfNumbers(numbersArray);
-		
+		for (int p = 0; p < numbersArray.length; p++) {
+			for (int i = 0; i < (numbersArray.length - 1); i++) {
+				int value1 = numbersArray[i];
+				int value2 = numbersArray[i + 1];
+				if (value1 < value2) {
+					numbersArray[i] = value2;
+					numbersArray[i + 1] = value1;
+				}
+			}
+		}
 		// sort numbers descending
 		// ... include code here
 		printArrayOfNumbers(numbersArray);
-		
+
 	}
-	
+
 	public static void printArrayOfNumbers(int[] numbers) {
-		
+
 		if (numbers == null || numbers.length == 0) {
 			System.out.println("empty list!");
 		} else {
--- src/com/dm/sort/NamesSortTemplate.java
+++ src/com/dm/sort/NamesSortTemplate.java
@@ -13,7 +13,7 @@ import java.io.PrintWriter;
  * 
  * Step 1. Read the first names and last names in two arrays of strings
  * 
- * Step 2. Generate an array of strings that includes 500 full names, and save them
+ * Step 2. Generate an array of strings that includes 10 full names, and save them
  * in a new file called "full-names.txt", one name per line
  * 
  * Step 3. Sort the array using bubble sort, and save to a new file called "sorted-names.txt"
@@ -23,7 +23,42 @@ import java.io.PrintWriter;
 public class NamesSortTemplate {
 	
 	public static void main(String[] args) throws Exception {
-		readWriteFile();
+		BufferedReader reader = new BufferedReader(new FileReader("data" + File.separator + "names.txt"));
+		PrintWriter writer = new PrintWriter("data" + File.separator + "names1.txt");
+		String firstNamesLine = reader.readLine();
+		String firstNames[] = firstNamesLine.split(",");
+		String lastNamesLine = reader.readLine();
+		String lastNames[] = lastNamesLine.split(",");
+		String fullNames[] = new String[10];
+		
+		for (int i = 0; i < fullNames.length; i++) {
+			fullNames[i] = firstNames[(int)(firstNames.length * Math.random())] + " " + lastNames[(int)(lastNames.length * Math.random())];
+			System.out.println(fullNames[i]);
+		}
+		
+		for (int p = 0; p < fullNames.length; p++) {
+			for (int i = 0; i < (fullNames.length - 1); i++) {
+				String value1 = fullNames[i];
+				String value2 = fullNames[i+1];
+				if (value1.compareToIgnoreCase(value2) > 0) {
+					fullNames[i] = value2;
+					fullNames[i+1] = value1;
+				}
+			}
+		}
+		
+		System.out.println( );
+		
+		for (int i = 0; i < fullNames.length; i++) {
+			System.out.println(fullNames[i]);
+			writer.println(fullNames[i]);
+		}
+		
+		
+		reader.close();
+		writer.close();
+		
+		
 	}
 
 	public static void readFile() throws Exception {
