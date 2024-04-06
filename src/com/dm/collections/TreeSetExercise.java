ae36610600c72cfa4cc658300e06e43eef52a3c7
sonia-o <sonicu.olar@gmail.com>
2024-04-06 17:44:50 -0400

    fixed error

--- src/com/dm/collections/TreeSetExercise.java
+++ src/com/dm/collections/TreeSetExercise.java
@@ -6,17 +6,17 @@ import java.util.TreeSet;
 
 public class TreeSetExercise {
 	public static void main(String[] args) {
-		HashSet<Student> set1 = new HashSet<Student>();
-		
-		Student sam = new Student("Sam", "Green");
-		Student john = new Student("John", "Smith");
-		Student alexia = new Student("Alexia", "Brown");
-		
-		set1.add(sam);
-		set1.add(john);
-		set1.add(alexia);
+//		HashSet<Student> set1 = new HashSet<Student>();
+//		
+//		Student sam = new Student("Sam", "Green");
+//		Student john = new Student("John", "Smith");
+//		Student alexia = new Student("Alexia", "Brown");
+//		
+//		set1.add(sam);
 //		set1.add(john);
-		
-		System.out.println(set1);
+//		set1.add(alexia);
+//		set1.add(john);
+//		
+//		System.out.println(set1);
 	}
 }
